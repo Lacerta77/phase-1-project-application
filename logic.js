f7064f5c4fbf3d348ccbb2e1f807eeffed82c5ca
lacerta <kevinhickman4@gmail.com>
2021-04-19 21:37:39 -0500

    fetched data and began fetching function

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
-fetch('https://api.the-odds-api.com/v3/odds/?apiKey=33ec130f94414bb4491dbddaeb8d3e0e&region=us&sport=americanfootball_nfl_super_bowl_winner&mkt=outrights')
+function fetchdata() {
+fetch('https://api.the-odds-api.com/v3/odds/?apiKey=33ec130f94414bb4491dbddaeb8d3e0e&sport=americanfootball_nfl_super_bowl_winner&region=us')
   .then(response => response.json())
   .then(data => console.log(data.data));
+}
